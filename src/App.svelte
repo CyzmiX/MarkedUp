2eaa3ab6445a0b072f1a7ff788dc0e17a212b898
CyzmiX <98268356+CyzmiX@users.noreply.github.com>
2023-10-20 21:09:53 +0100

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,58 @@
+<script>
+	let name = 'CyzmiX'
+	let doc = ''
+	let show = false
+	import { marked } from 'marked'
+</script>
+
+<main>
+	<textarea on:change={(e) => doc = e.target.value} />
+	<h3>Preview</h3>
+	<iframe title="main" srcdoc={marked.parse(doc)}></iframe>
+
+	<button on:click={() => { 
+		navigator.clipboard.writeText(marked.parse(doc)) 
+	}}>Copy Html</button>
+</main>
+
+<style>
+	textarea {
+		color: black;
+		width: 100%;
+		height: 150px;
+		padding: 12px 20px;
+		box-sizing: border-box;
+		border: 2px solid #ccc;
+		border-radius: 4px;
+		background-color: #f8f8f8;
+		font-size: 16px;
+		resize: none;
+		font-size: 18px;
+	}
+
+	h3 {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	button {
+		background-color: rgb(200, 200, 215);
+		border: 1.5px solid rgb(38, 33, 33);
+		border-radius: 10px;
+		color: black;
+
+		transition: border-radius 0.35s;
+	}
+
+	button:hover {
+		border-radius: 14px;
+	}
+
+	iframe {
+		width: 100%;
+		height: 150px;
+		padding: 12px 20px;
+		box-sizing: border-box;
+	}
+</style>
